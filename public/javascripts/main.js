dc3b7f8e9c9a402f3912c20d7ace2695c7b0cf0f
kauredo <vaskafig@gmail.com>
2025-02-06 19:18:56 +0000

    styles and js

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+// Show the loading screen when the form is submitted
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loading-screen").style.display = "none";
+
+  const form = document.getElementById("invite-form");
+  if (form) {
+    form.addEventListener("submit", function () {
+      document.getElementById("loading-screen").style.display = "flex";
+    });
+  }
+});
