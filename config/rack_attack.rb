e9ac147dbe79a65e10296093e34efa91dc6cbfdc
kauredo <vaskafig@gmail.com>
2025-02-07 11:22:47 +0000

    fix rack attack

--- config/rack_attack.rb
+++ config/rack_attack.rb
@@ -1,16 +1,6 @@
 class Rack::Attack
   # Throttle requests by IP address (5 requests per minute)
-  throttle('req/ip', limit: 2, period: 60) do |req|
+  throttle('req/ip', limit: 5, period: 60) do |req|
     req.ip
-  end
-
-  # Blocklist specific IPs (optional)
-  blocklist('block bad IP') do |req|
-    ['192.168.1.100', '10.0.0.1'].include?(req.ip)
-  end
-
-  # Whitelist specific IPs (optional)
-  safelist('allow local IPs') do |req|
-    ['127.0.0.1', '::1'].include?(req.ip)
   end
 end
